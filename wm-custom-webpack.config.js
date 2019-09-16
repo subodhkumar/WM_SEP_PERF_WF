0d4bb8b725bda8b25fd0c9da2d39ca5d50463926
Subodh Kumar ES <subodh.kumar@wavemaker.com>
2019-09-16 10:41:57 +0530

    Brotli Compression Only

--- wm-custom-webpack.config.js
+++ wm-custom-webpack.config.js
@@ -12,15 +12,15 @@ module.exports = {
             asset: '[path].br[query]',
             test: /\.(js|css|html|svg|txt|eot|otf|ttf|gif)$/
         }),
-        new CompressionPlugin({
-            test: /\.(js|css|html|svg|txt|eot|otf|ttf|gif)$/,
-            filename(info){
-                let opFile= info.path.split('.'),
-                opFileType =  opFile.pop(),
-                opFileName = opFile.join('.');
-                return `${opFileName}.gz.${opFileType}`;
-            }
-        })
+        // new CompressionPlugin({
+        //     test: /\.(js|css|html|svg|txt|eot|otf|ttf|gif)$/,
+        //     filename(info){
+        //         let opFile= info.path.split('.'),
+        //         opFileType =  opFile.pop(),
+        //         opFileName = opFile.join('.');
+        //         return `${opFileName}.gz.${opFileType}`;
+        //     }
+        // })
     ],
     optimization: {
         splitChunks: {
